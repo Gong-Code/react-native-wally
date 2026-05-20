5396d4d8eb477c58cd7d84d21803248ba286f0e5
Gong Moonphruk <gong.moonphruk@gmail.com>
2026-05-20 15:03:17 +0200

    feat: project setup, nativewind, routing, tab navigation

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -2,6 +2,6 @@ import { Stack } from "expo-router";
 
 import "@/global.css";
 
-export default function RootLayout() {
+export default function AuthLayout() {
   return <Stack screenOptions={{ headerShown: false }} />;
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,13 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 import "@/global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar style="dark" />
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  );
 }
